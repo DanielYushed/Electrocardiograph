c50bfaada92aa0339851e9bfd7ea9c8d54a3739a
DanielYushed <danielyushed14@gmail.com>
2024-03-04 18:41:12 -0600

    Se agrega el proyecto

---
+++ Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace Electrocardiografo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.circularProgressBar1 = new CircularProgressBar.CircularProgressBar();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Plantilla = new System.Windows.Forms.PictureBox();
+            this.Conectar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBoxBaudRate = new System.Windows.Forms.ComboBox();
+            this.comboBoxPuerto = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.s = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Plantilla)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.circularProgressBar1);
+            this.panel1.Location = new System.Drawing.Point(585, 245);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(206, 195);
+            this.panel1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(80, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 36);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "0";
+            // 
+            // circularProgressBar1
+            // 
+            this.circularProgressBar1.AnimationFunction = WinFormAnimation.KnownAnimationFunctions.Liner;
+            this.circularProgressBar1.AnimationSpeed = 20;
+            this.circularProgressBar1.BackColor = System.Drawing.Color.Transparent;
+            this.circularProgressBar1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.circularProgressBar1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.circularProgressBar1.InnerColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.circularProgressBar1.InnerMargin = 2;
+            this.circularProgressBar1.InnerWidth = -1;
+            this.circularProgressBar1.Location = new System.Drawing.Point(19, 14);
+            this.circularProgressBar1.MarqueeAnimationSpeed = 1000;
+            this.circularProgressBar1.Maximum = 10;
+            this.circularProgressBar1.Name = "circularProgressBar1";
+            this.circularProgressBar1.OuterColor = System.Drawing.Color.Gray;
+            this.circularProgressBar1.OuterMargin = -25;
+            this.circularProgressBar1.OuterWidth = 26;
+            this.circularProgressBar1.ProgressColor = System.Drawing.Color.Cyan;
+            this.circularProgressBar1.ProgressWidth = 10;
+            this.circularProgressBar1.SecondaryFont = new System.Drawing.Font("Microsoft Sans Serif", 36F);
+            this.circularProgressBar1.Size = new System.Drawing.Size(168, 167);
+            this.circularProgressBar1.StartAngle = 270;
+            this.circularProgressBar1.Step = 1;
+            this.circularProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.circularProgressBar1.SubscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(166)))), ((int)(((byte)(166)))));
+            this.circularProgressBar1.SubscriptMargin = new System.Windows.Forms.Padding(10, -35, 0, 0);
+            this.circularProgressBar1.SubscriptText = "";
+            this.circularProgressBar1.SuperscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(166)))), ((int)(((byte)(166)))));
+            this.circularProgressBar1.SuperscriptMargin = new System.Windows.Forms.Padding(10, 35, 0, 0);
+            this.circularProgressBar1.SuperscriptText = "";
+            this.circularProgressBar1.TabIndex = 0;
+            this.circularProgressBar1.Text = "LPM";
+            this.circularProgressBar1.TextMargin = new System.Windows.Forms.Padding(2, -15, 0, 0);
+            this.circularProgressBar1.UseWaitCursor = true;
+            this.circularProgressBar1.Value = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.panel2.Controls.Add(this.Plantilla);
+            this.panel2.Location = new System.Drawing.Point(12, 167);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(567, 273);
+            this.panel2.TabIndex = 1;
+            // 
+            // Plantilla
+            // 
+            this.Plantilla.BackColor = System.Drawing.Color.Black;
+            this.Plantilla.Location = new System.Drawing.Point(16, 15);
+            this.Plantilla.Name = "Plantilla";
+            this.Plantilla.Size = new System.Drawing.Size(537, 244);
+            this.Plantilla.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Plantilla.TabIndex = 5;
+            this.Plantilla.TabStop = false;
+            // 
+            // Conectar
+            // 
+            this.Conectar.BackColor = System.Drawing.Color.Cyan;
+            this.Conectar.Font = new System.Drawing.Font("Sylfaen", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Conectar.ForeColor = System.Drawing.Color.Black;
+            this.Conectar.Location = new System.Drawing.Point(294, 31);
+            this.Conectar.Name = "Conectar";
+            this.Conectar.Size = new System.Drawing.Size(129, 78);
+            this.Conectar.TabIndex = 2;
+            this.Conectar.Text = "Conectar";
+            this.Conectar.UseVisualStyleBackColor = false;
+            this.Conectar.Click += new System.EventHandler(this.Conectar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(68)))), ((int)(((byte)(68)))));
+            this.groupBox1.Controls.Add(this.comboBoxBaudRate);
+            this.groupBox1.Controls.Add(this.Conectar);
+            this.groupBox1.Controls.Add(this.comboBoxPuerto);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Stencil", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.groupBox1.Location = new System.Drawing.Point(122, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(457, 133);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Conexion";
+            // 
+            // comboBoxBaudRate
+            // 
+            this.comboBoxBaudRate.Font = new System.Drawing.Font("Symbol", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.comboBoxBaudRate.FormattingEnabled = true;
+            this.comboBoxBaudRate.Location = new System.Drawing.Point(132, 77);
+            this.comboBoxBaudRate.Name = "comboBoxBaudRate";
+            this.comboBoxBaudRate.Size = new System.Drawing.Size(121, 32);
+            this.comboBoxBaudRate.TabIndex = 7;
+            // 
+            // comboBoxPuerto
+            // 
+            this.comboBoxPuerto.Font = new System.Drawing.Font("Symbol", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.comboBoxPuerto.FormattingEnabled = true;
+            this.comboBoxPuerto.Location = new System.Drawing.Point(132, 30);
+            this.comboBoxPuerto.Name = "comboBoxPuerto";
+            this.comboBoxPuerto.Size = new System.Drawing.Size(121, 32);
+            this.comboBoxPuerto.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Stencil", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 21);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Baudrate";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Stencil", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 21);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Puerto";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // s
+            // 
+            this.s.BaudRate = 250000;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Electrocardiografo.Properties.Resources._1fac0;
+            this.pictureBox2.Location = new System.Drawing.Point(588, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(203, 212);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Electrocardiografo.Properties.Resources.pulse_line_red_1;
+            this.pictureBox1.Location = new System.Drawing.Point(11, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(105, 121);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(68)))), ((int)(((byte)(68)))));
+            this.ClientSize = new System.Drawing.Size(808, 458);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Plantilla)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        public System.Windows.Forms.PictureBox Plantilla;
+        private System.Windows.Forms.Button Conectar;
+        private CircularProgressBar.CircularProgressBar circularProgressBar1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBoxBaudRate;
+        private System.Windows.Forms.ComboBox comboBoxPuerto;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.IO.Ports.SerialPort s;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,136 @@
+﻿/*
+ *  Interfaz para electrocardiograma (ECG) v3.5.11 (estable)
+ *
+ *  Docente: Varela Lara Luis Julián
+ *  Equipo: Teletobbies
+ *
+ *  Interfaz realizada para trabajo final de materia Tópicos avanzados de INEL II
+ *  realizada por alumnos de la Universidad Veracruzana (UV)
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Electrocardiografo
+{
+    public partial class Form1 : Form
+    {
+        Bitmap Papel;                               // Inicializar grafica
+        Pen Lapiz, Borrador;                        // Inicializar lapices
+        Graphics Dibujante;                         // Inicializar dibujante
+
+        Double ECG = 0, ECGAnterior = 0;            // Inicializar variables para guardar la señal
+
+        int Tiempo = 0;                             // Inicializar iterador del eje tiempo
+        int Segundos = 0;                           // Inicializar iterador de tiempo en segundos
+        int picos = 1;                              // Inicializar contador de latidos
+        int band = 0;                               // Inicializar bandera para estado de la imagen de corazon 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //////////////////////////////////<-	Boton de conectar   ->/////////////////////////////////////
+        private void Conectar_Click(object sender, EventArgs e)
+        {
+            s.BaudRate = Convert.ToInt32(comboBoxBaudRate.Text);        // Convertir datos de comboBox y asignarlo
+            s.PortName = comboBoxPuerto.Text;                           // Asignar puerto disponible
+            s.Open();                                                   // Activar comunicacion serial
+
+            circularProgressBar1.Style= ProgressBarStyle.Continuous;    // Cambiar de modo a grafica circular
+
+            timer2.Enabled = true;                                      // Activar Timers
+            timer1.Enabled = true;
+            
+        }
+
+        //////////////////////////<-	Impresion de datos en grafica   ->////////////////////////////////
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int Dato;                                                   // Variable auxiliar
+
+            while (s.BytesToRead > 0)                                   // Mientras haya datos por leer
+            {
+                Dato = s.ReadByte();                                    // Guardar valor en variable
+                ECG = Dato;                                             
+                Dibujante.DrawLine(Borrador, Tiempo, 0, Tiempo, 600);                                   // Parametros para grafica
+                Dibujante.DrawLine(Lapiz, Tiempo - 1, (int)ECGAnterior + 20, Tiempo, (int)ECG + 20);
+                Tiempo++;                                               // Incrementar valor de tiempo en 1
+
+                if (ECG == 0 && ECGAnterior != 0)                       // Detectar caida de latido
+                {
+                    picos = picos + 1;                                  // Incrementar valor de los latidos registrados
+                }
+
+                if (Segundos >= 10)                                     // ¿Pasaron 10 segundos?
+                {
+                    picos = picos*6;                                    // Calcular latidos por minuto
+                    label3.Text = picos.ToString();                     // Imprimir latidos por minuto
+
+                    Tiempo = 0;                                         // Refrescar datos
+                    Segundos = 0;
+                    picos = 1;
+                }
+
+                Plantilla.Image = Papel;                                // Refrescar plantilla
+
+                ECGAnterior = ECG;                                      // Guardar dato anterior
+            }
+        }
+
+        //////////////////////////////////<-	Cronometro   ->/////////////////////////////////////
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Segundos = Segundos + 1;                                    // Incrementar valor de tiempo en 1
+            circularProgressBar1.Value = Convert.ToInt32(Segundos);     // Enviar datos a grafica circular
+
+            if (band == 0)                              // Alternar la aparicion del corazon 
+            {
+                pictureBox2.Visible = true;
+                band= 1;
+            }
+            else
+            {
+                pictureBox2.Visible = false; 
+                band= 0;
+            }
+        }
+
+        //////////////////////////////////<-	Parametros iniciales   ->/////////////////////////////////////
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {       
+                Lapiz = new Pen(Color.LightSkyBlue, 2);                                 // Inicializar parametros para graficar
+                Borrador = new Pen(Color.Black, 1);
+                Papel = new Bitmap(2500, 200);
+                Dibujante = Graphics.FromImage(Papel);
+
+                Plantilla.Image = Papel;                                                // Refrescar plantilla
+
+                string[] portList = SerialPort.GetPortNames();                          // Buscar y guardar puertos disponibles
+                comboBoxPuerto.Items.AddRange(portList);                                // Añadir puertos encontrados
+
+                string[] rates = {"9600", "38400", "57600", "115200", "250000"};        // Guardar velocidades permitidas
+                comboBoxBaudRate.DataSource = rates;                                    // Añadir velocidades
+            }
+
+            catch (Exception error)                                                     // ¿Ocurrio algun error?
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+    }
+}
